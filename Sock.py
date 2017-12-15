9f1f3e5f751a68038b9ea935505a24a7bebe85ee
Bret Gourdie <bretgourdie@gmail.com>
2017-12-15 15:36:57 -0500

    Indent correctly

--- Sock.py
+++ Sock.py
@@ -1,11 +1,11 @@
 class Sock:
 
     def __init__(self, color=None, length=None, material=None, pattern=None, size=None):
-    self.color = color
-    self.length = length
-    self.material = material
-    self.pattern = pattern
-    self.size = size
+        self.color = color
+        self.length = length
+        self.material = material
+        self.pattern = pattern
+        self.size = size
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
