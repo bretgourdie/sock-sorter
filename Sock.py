0880c3216116aa52680003e2d70adbe85f1747bd
Bret Gourdie <bretgourdie@gmail.com>
2017-12-15 15:27:09 -0500

    Correct Sock to use __init__

--- Sock.py
+++ Sock.py
@@ -1,10 +1,11 @@
 class Sock:
 
-    color = None
-    length = None
-    material = None
-    pattern = None
-    size = None
+    def __init__(self, color=None, length=None, material=None, pattern=None, size=None):
+    self.color = color
+    self.length = length
+    self.material = material
+    self.pattern = pattern
+    self.size = size
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
