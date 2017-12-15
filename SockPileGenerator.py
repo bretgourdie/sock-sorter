ec0883001cb35a04f349d7695481710bc0f53386
Bret Gourdie <bretgourdie@gmail.com>
2017-12-15 15:51:17 -0500

    Reorganize methods

--- SockPileGenerator.py
+++ SockPileGenerator.py
@@ -1,9 +1,23 @@
-import inspect 
+from Sock import Sock
+import inspect, SockEnums
 
 class SockPileGenerator:
 
     def __init__(self):
         1 + 1
+
+    def getRandomEnum(self, propertyName):
+        for name, obj in inspect.getmembers(SockEnums):
+            if inspect.isclass(obj) and propertyName.title() == name:
+                return random.choice(list(obj))
+
+    def generateSock(self, templateSock):
+        sock = Sock()
+
+        for property, value in vars(templateSock).iteritems():
+            setattr(sock, value or getRandomEnum(property))
+
+        return sock
 
     def generatePile(self, numberOfSocks, templateSock):
         sockPile = []
@@ -22,15 +36,3 @@ class SockPileGenerator:
 
         return sockPile
 
-    def generateSock(self, templateSock):
-        sock = Sock()
-
-        for property, value in vars(templateSock).iteritems():
-            setattr(sock, value or getRandomEnum(property))
-
-        return sock
-
-    def getRandomEnum(self, propertyName):
-        for name, obj in inspect.getmembers(SockEnums):
-            if inspect.isclass(obj) and propertyName.title() == name:
-                return random.choice(list(obj))
