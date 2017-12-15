250c30fc0a07caece1b1e893e1e2dc1718f1885d
Bret Gourdie <bretgourdie@gmail.com>
2017-12-15 15:23:31 -0500

    Remove "self"; self has no scope here

--- Sock.py
+++ Sock.py
@@ -1,10 +1,10 @@
 class Sock:
 
-    self.color = None
-    self.length = None
-    self.material = None
-    self.pattern = None
-    self.size = None
+    color = None
+    length = None
+    material = None
+    pattern = None
+    size = None
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
