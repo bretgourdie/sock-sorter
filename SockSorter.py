20a90e06e94f87e4d24714a94da30ccf07dc5101
Bret Gourdie <bretgourdie@gmail.com>
2017-12-15 16:20:29 -0500

    Include run function

--- SockSorter.py
+++ SockSorter.py
@@ -1,14 +1,16 @@
 import Sock, SockEnums, SockPileGenerator
 
 class SockSorter():
-    sockPileGenerator = SockPileGenerator.SockPileGenerator()
 
-    templateSock = Sock.Sock(None, SockEnums.Length.Ankle, SockEnums.Material.Cotton, SockEnums.Pattern.Plain, SockEnums.Size.Medium)
+    def run(self):
+        sockPileGenerator = SockPileGenerator.SockPileGenerator()
 
-    pile = sockPileGenerator.generatePile(10, templateSock)
+        templateSock = Sock.Sock(None, SockEnums.Length.Ankle, SockEnums.Material.Cotton, SockEnums.Pattern.Plain, SockEnums.Size.Medium)
 
-    for sock in pile:
-        print(sock)
+        pile = sockPileGenerator.generatePile(10, templateSock)
+
+        for sock in pile:
+            print(sock)
 
 if __name__ == "__main__":
     app = SockSorter()
