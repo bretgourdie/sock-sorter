cadd68b5fe4026fe07cc661fa603834062a76836
Bret Gourdie <bretgourdie@gmail.com>
2017-12-20 17:09:27 -0500

    Implement Selection Sort

---
+++ SelectionSort.py
@@ -0,0 +1,22 @@
+from Sock import Sock
+from SortingMethod import SortingMethod
+
+class SelectionSort(SortingMethod):
+
+    def sort(self, sockPile):
+        
+        sortedPile = []
+
+        while len(sockPile) > 0:
+            leftSock = sockPile.pop(0)
+
+            rightSock = Sock()
+            while leftSock != rightSock:
+                rightSock = sockPile.pop(0)
+                
+                if leftSock != rightSock:
+                    sockPile.append(rightSock)
+
+            sortedPile.append((leftSock, rightSock))
+
+        return sortedPile
